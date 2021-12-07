da7fa3e69e9b5d17eb2d91f5b21aefd00b323490
akakiplimo <adriankiplimo@gmail.com>
2021-12-07 10:56:16 +0000

    Print list with exception

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """function that prints x elements of a list"""
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError, IndexError):
+            break
+    print()
+    return count
+        
+            
