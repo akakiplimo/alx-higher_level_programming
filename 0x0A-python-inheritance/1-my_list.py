a049540888ef06f34f04aeb784ebd6b4f6d5207f
akakiplimo <adriankiplimo@gmail.com>
2021-12-20 14:00:01 +0000

    MyList

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines MyList Class"""
+class MyList(list):
+    """A subclass of list"""
+    def __init__(self):
+        """initializes the list"""
+        super().__init__()
+
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        print(sorted(self))
