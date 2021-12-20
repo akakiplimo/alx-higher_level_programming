ed50950c9605283e5b5f276eb1c2911334ed9355
akakiplimo <adriankiplimo@gmail.com>
2021-12-20 14:22:04 +0000

    remove if statement

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,4 @@
 def is_same_class(obj, a_class):
     """" returns True if the object is exactly 
     an instance of the specified class, Otherwise False"""
-    if (type(obj) == a_class):
-        return True
-    return False
+    return (type(obj) == a_class)
