ae4c00c2c2838e8c30a9be3e9ebcfc3f6ab9de62
akakiplimo <adriankiplimo@gmail.com>
2021-12-21 21:01:04 +0000

    removed whitespace

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file in UTF8
-    and returns the number of characters written"""    
+    and returns the number of characters written"""
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 
 
 def append_write(filename="", text=""):
-    """function that appends a string to the end of a text 
+    """function that appends a string to the end of a text
     file in UTF8 and returns the number of characters added"""
 
     with open(filename, 'a', encoding='utf-8') as f:
