f77dc722c3a7a62736cd46b524dc7ff2dd21f667
akakiplimo <adriankiplimo@gmail.com>
2021-12-21 20:36:49 +0000

    append_write with utf-8

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""module that defines a function append_write"""
+
+
+def append_write(filename="", text=""):
+    """function that appends a string to the end of a text 
+    file in UTF8 and returns the number of characters added"""
+
+    with open(filename, 'a', encoding='utf-8') as f:
+        return f.write(text)
