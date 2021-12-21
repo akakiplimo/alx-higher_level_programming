44a5ec3fec067f75c18e0cf81fd9fbe8b11d3f99
akakiplimo <adriankiplimo@gmail.com>
2021-12-21 20:25:28 +0000

    write file in utf-8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,6 @@
 
 def write_file(filename="", text=""):
     """function that writes a string to a text file in UTF8
-    and returns the number of characters written"""
-    
-    with open(filename, 'w', encoding="UTF8") as f:
+    and returns the number of characters written"""    
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
