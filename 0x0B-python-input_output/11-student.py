6c4e9548f1711d62ccddcbd6d35bb0d1238409a5
akakiplimo <adriankiplimo@gmail.com>
2021-12-22 22:04:55 +0000

    py input output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Module that defines class Student"""
+
+
+class Student:
+    """class that represents a Student"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation params"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for key in json:
+            try:
+                setattr(self, key, json[key])
+            except:
+                pass
