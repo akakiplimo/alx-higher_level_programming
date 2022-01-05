713fb5bb40e695a889db22353dad281ee767b084
akakiplimo <adriankiplimo@gmail.com>
2022-01-05 04:44:56 +0000

    PEP8 fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,9 +6,9 @@ class Base:
     """ represents a class Base """
     __nb_objects = 0
 
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         """ class construtor that initializes the class """
-        if (id != None):
+        if (id is not None):
             self.id = id
         else:
             Base.__nb_objects += 1
