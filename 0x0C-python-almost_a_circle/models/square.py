82ce811354fae676701259ff63b79b005d096ee3
akakiplimo <adriankiplimo@gmail.com>
2022-01-07 14:20:04 +0000

    PEP 8 fix

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,24 +8,24 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """ represents a square object """
     def __init__(self, size, x=0, y=0, id=None):
-      """ initialize the Square """
-      super().__init__(size, size, x, y, id)
-      self.size = size
+        """ initialize the Square """
+        super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
-      """ size getter """
-      return self.height
+        """ size getter """
+        return self.height
 
     @size.setter
     def size(self, value):
-      """ size setter """
-      self.width = value
-      self.height = value
+        """ size setter """
+        self.width = value
+        self.height = value
 
     def __str__(self):
-      """ string representation of Square """
-      return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                       self.x,
-                                                       self.y,
-                                                       self.width)
+        """ string representation of Square """
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
+                                                         self.x,
+                                                         self.y,
+                                                         self.width)
