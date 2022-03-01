f09344b426703b49726213908c9a404ee5f8cc2d
akakiplimo <adriankiplimo@gmail.com>
2022-03-01 11:23:40 +0000

    nboccurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  for (const num in list) {
+    if ( list[num] === searchElement) {
+      i++
+    }
+  }
+  return (i);
+};
