a55fd769ee983958de3ace888b4e77461e646728
akakiplimo <adriankiplimo@gmail.com>
2022-03-01 09:42:54 +0000

    define rectangle if w & h are positive integers

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor(w, h){
+    // if w & h are positive integers create Rectangle otherwise create empty Rectangle
+    if (w > 0 && h > 0)
+    {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
