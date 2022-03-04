7298ae128cb56c20cb88fcaafef24a7b43e99a7e
akakiplimo <adriankiplimo@gmail.com>
2022-03-04 08:04:05 +0000

    filter cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+python script that lists all cities from the database hbtn_0e_4_usa
+with specified state name
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id=states.id WHERE \
+    states.name LIKE %s ORDER BY cities.id", (sys.argv[4],))
+    query_rows = cur.fetchall()
+    print(", ".join(city[0] for city in query_rows))
+    cur.close()
+    conn.close()
