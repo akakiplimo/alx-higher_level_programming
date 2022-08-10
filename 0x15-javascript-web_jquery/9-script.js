14d3f0f93ead34393166aa45561314ca895d1a5d
A. Kiplimo <akakiplimo@gmail.com>
2022-08-10 16:12:04 +0300

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
-        if (status === "success") {
-            $('#hello').text(data.hello);
-        }
-    });
-});
