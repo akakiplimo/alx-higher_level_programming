d1f529fd715f7674c39b039a7b97d9f082f4c5f0
A. Kiplimo <adriankiplimo@gmail.com>
2022-08-10 16:08:59 +0300

    Delete 0x0E-SQL_more_queries directory

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++
@@ -1,8 +0,0 @@
--- 13-count_shows_by_genre.sql
-SELECT tv_genres.name AS genre,
-       COUNT(show_genres.show_id) AS number_of_shows
-FROM tv_genres
-INNER JOIN tv_show_genres AS show_genres
-ON tv_genres.id = show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC
