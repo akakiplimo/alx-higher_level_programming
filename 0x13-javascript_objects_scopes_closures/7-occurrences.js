d8e7239713699896d4bf6f39dad56c6a43b95c7e
akakiplimo <akakiplimo@gmail.com>
2022-09-06 03:33:11 -0700

    count occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  for (const item in list) {
+    if (list[item] === searchElement) { count++; }
+  }
+
+  return count;
+};
