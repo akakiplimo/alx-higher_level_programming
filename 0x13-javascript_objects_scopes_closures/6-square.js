231a55ff2d858dc1de5e75fe5bc0b2f96ad9f7ad
akakiplimo <akakiplimo@gmail.com>
2022-09-06 03:32:43 -0700

    Sqaure prints

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const SquareParent = require('./5-square');
+
+// inheriting parent class
+class Square extends SquareParent {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
