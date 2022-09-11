d8566ec08704437995934b77b5c0cb244f0fb9fc
akakiplimo <akakiplimo@gmail.com>
2022-09-11 04:33:49 -0700

    fix pycodestyle

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa"""
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 
 
 import MySQLdb
@@ -37,3 +39,4 @@ if __name__ == '__main__':
         print(state)
     cursor.close()
     db.close()
+
